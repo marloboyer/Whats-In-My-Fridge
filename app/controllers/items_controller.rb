e1c48bdd10ab4e484214b3eda7817263f774fc03
marloboyer <marlo.boyer7@yahoo.com>
2023-03-28 21:40:16 -0500

    Added a create action to Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,14 @@ class ItemsController < ApplicationController
     @items = Item.all
     render :index
   end
+
+  def create
+    @item = Item.create(
+      name: params[:name],
+      category: params[:category],
+      lives_at: params[:lives_at],
+      best_by: params[:best_by],
+    )
+    render :show
+  end
 end
--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -8,4 +8,11 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Item.count, data.length
   end
+
+  test "create" do
+    assert_difference "Item.count", 1 do
+      post "/items.json", params: { name: "Eggs", category: "Poultry", lives_At: "Frige", best_by: "3/25/2023" }
+      assert_response 200
+    end
+  end
 end
